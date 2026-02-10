17f69e23d7e3ff889b69f075411e31addaf90965
wangjw <wjwei113@msn.com>
2026-02-10 17:24:55 +0800

    增加页面

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
+/*
+ * @Author: Floppy
+ * @LastEditors: Floppy
+ * @Description: 
+ */
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = '欢迎来到我的网站!';
+export const SITE_TITLE = '深圳佰易仪器设备有限公司';
+export const SITE_DESCRIPTION = '佰易智能是一家国内领先的专业智能装备制造和服务商，流体智能装备生产线，高效率高质量提供自动点胶机，精密点胶机，桌面型点胶机及自动灌胶机等设备，自动点胶机及自动自动灌胶机';
