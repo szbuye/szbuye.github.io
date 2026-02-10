826163c520cb2bbbaae126ff6d41ee32e2e37a91
wangjw <wjwei113@msn.com>
2026-02-10 17:46:21 +0800

    修改元数据

--- src/consts.ts
+++ src/consts.ts
@@ -8,3 +8,5 @@
 
 export const SITE_TITLE = '深圳佰易仪器设备有限公司';
 export const SITE_DESCRIPTION = '佰易智能是一家国内领先的专业智能装备制造和服务商，流体智能装备生产线，高效率高质量提供自动点胶机，精密点胶机，桌面型点胶机及自动灌胶机等设备，自动点胶机及自动自动灌胶机';
+export const SITE_URL = 'https://szbuye.vispace.top';
+export const SITE_AUTH = 'Floppy';
